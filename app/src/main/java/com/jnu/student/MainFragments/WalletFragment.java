0f74d24d6d19bff28b438aad9dbfe4392fc0318c
Bayoulove1 <1605289866@qq.com>
2023-12-16 22:41:20 +0800

    完成“钱包”功能的实现，并且可以记录点击了完成任务添加按钮的任务的Score。

--- app/src/main/java/com/jnu/student/MainFragments/WalletFragment.java
+++ app/src/main/java/com/jnu/student/MainFragments/WalletFragment.java
@@ -4,13 +4,19 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.jnu.student.DataKeep.GainScoreDataBank;
 import com.jnu.student.R;
 
 public class WalletFragment extends Fragment {
+    private double TotalScore;
+    private TextView textViewScore;
+    private Handler handler = new Handler();
 
     public WalletFragment() {
         // Required empty public constructor
@@ -35,6 +41,37 @@ public class WalletFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_wallet, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_wallet, container, false);
+        textViewScore = rootView.findViewById(R.id.text_wallet_data);//没问题这里，因为要获得的是分数显示的数字
+        return rootView;
     }
+    public void onResume(){
+        super.onResume();
+        startTextViewUpdate();
+    }
+    //6,第一次遇到写函数忘记加括号的笨蛋，高，实在是高
+    private void startTextViewUpdate(){
+        handler.post(updateScore);
+    }
+    public void onPause(){
+        super.onPause();
+        stopTextViewUpdate();
+    }
+
+    private void stopTextViewUpdate(){
+        handler.removeCallbacks(updateScore);
+    }
+
+    //通过运行时间更新内容（很巧妙）
+    private Runnable updateScore = new Runnable() {
+        @Override
+        public void run() {
+           TotalScore = new GainScoreDataBank().WalletLoad(requireActivity());
+           textViewScore.setText(String.valueOf(TotalScore));
+           //更新的时间间隔
+            handler.postDelayed(this,1000);
+
+        }
+    };
+
 }
